7c3a812bebf974f924d3794c9c64800188cb98f3
markspooner1 <markaustinspooner@gmail.com>
2023-03-24 11:23:10 -0400

    update projects

--- src/Page/Projects.js
+++ src/Page/Projects.js
@@ -3,11 +3,13 @@ import "../styles/Project.css"
 import proj1 from "../assets/RMC.png"
 import proj2 from "../assets/logo.png"
 import proj3 from "../assets/database.png"
+import proj4 from "../assets/8puzzle.png"
 const Projects = () => {
     return ( <div className="projects">
         <h1>Projects</h1>
         <div className="projectList">
             <ProjectItem name ="Rate My Courses" image = {proj1} url = "https://github.com/markspooner1/Rate-My-Courses-MERN-App"/>
+            <ProjectItem name ="8 Puzzle Solver" image = {proj4} url = "https://github.com/markspooner1/8PuzzleVisualizer" />
             <ProjectItem name ="Warzone game" image = {proj2} url = "https://github.com/markspooner1/Warzone-Risk-Project"/>
             <ProjectItem name = "COVID Database management system" image = {proj3} url = "https://github.com/markspooner1/Covid-Database-Management-website"/>
         </div>
